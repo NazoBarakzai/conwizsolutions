5e5d32e3a98756fdf3365ce60a01f6d993eadd78
ConWiz-Solutions <73035678+NazoBarakzai@users.noreply.github.com>
2025-11-28 16:00:29 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -12,4 +12,28 @@
         });
         hamburger.addEventListener('mouseup', () => {
             hamburger.style.transform = 'scale(1)';
+
+        }); function showAlert() {
+            alert("Welcome to ConwizSolutions — Let's build the future together!");
+        }
+
+        function toggleMenu() {
+            document.querySelector('nav').classList.toggle('show');
+        }
+
+        const hamburger = document.querySelector('.hamburger');
+        hamburger.addEventListener('mousedown', () => {
+            hamburger.style.transform = 'scale(0.85)';
         });
+        hamburger.addEventListener('mouseup', () => {
+            hamburger.style.transform = 'scale(1)';
+        });
+        function openModal(imageSrc, title) {
+    document.getElementById("modalImg").src = imageSrc;
+    document.getElementById("caption").innerText = title;
+    document.getElementById("imageModal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("imageModal").style.display = "none";
+}
